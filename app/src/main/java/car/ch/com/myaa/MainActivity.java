5bcd50eafb328223b84484a5a1b64b0c59ca6410
StillTmac <13165552818@163.com>
2016-12-14 19:13:19 +0800

    Tracy

--- app/src/main/java/car/ch/com/myaa/MainActivity.java
+++ app/src/main/java/car/ch/com/myaa/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("哈哈哈");
+        System.out.println("依然");
+
     }
 }
