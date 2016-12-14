c7ee4bb232850cfefeea0f9f24c8ebff28a0eda4
ch1991 <changhua0610@163.com>
2016-12-14 19:14:12 +0800

    hua

--- app/src/main/java/car/ch/com/myaa/MainActivity.java
+++ app/src/main/java/car/ch/com/myaa/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("哈哈哈");
+
+
     }
 }
